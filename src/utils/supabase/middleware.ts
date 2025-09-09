96807852bbb9e030d849723f97aedcab4007d3b3
DmitriyGulevich <kaskodim@gmail.com>
2025-09-09 15:28:30 +0300

    test public #38

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -40,7 +40,8 @@ export async function updateSession(request: NextRequest) {
       !user &&
       !request.nextUrl.pathname.startsWith("/login") &&
       !request.nextUrl.pathname.startsWith("/auth") &&
-      !request.nextUrl.pathname.startsWith("/error")
+      !request.nextUrl.pathname.startsWith("/error") &&
+      !request.nextUrl.pathname.startsWith("/public")
     ) {
       // no user, potentially respond by redirecting the user to the login page
       const url = request.nextUrl.clone();
