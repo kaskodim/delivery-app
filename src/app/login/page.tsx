31cdf6f8301c5975f273cb0a4969aa7079a4fd5e
DmitriyGulevich <kaskodim@gmail.com>
2025-09-09 13:00:24 +0300

    добавляет страницу логина #38

--- src/app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Typography } from "@mui/material";
-
-const LoginPage = () => {
-  return <Typography>На этой странице будет аутентификация (форма входа)</Typography>;
-};
-
-export default LoginPage;
