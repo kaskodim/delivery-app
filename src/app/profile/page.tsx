f2b11cbb03a9a2869f00e632c000cbb5fec88300
Гулевич Екатерина <meteorgul@gmail.com>
2025-09-10 14:16:12 +0300

    исправление бага с мерцанием темы и соотвествующей ошибки от nextjs #56

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,13 @@
 "use server";
 
 import React from "react";
-import { Typography } from "@mui/material";
+import { AppBar, Typography } from "@mui/material";
 import ThemeSelector from "@/components/ThemeSelector";
 
 const ProfilePage = () => {
   return (
     <div className={"flex justify-between w-full"}>
+      <AppBar position="static">header</AppBar>
       <Typography>Страница профиля</Typography>
       <ThemeSelector />
     </div>
--- src/providers/themeContext/CustomThemeProvider.tsx
+++ src/providers/themeContext/CustomThemeProvider.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { FC, PropsWithChildren, useCallback, useMemo, useState } from "react";
+import { FC, PropsWithChildren, useCallback, useEffect, useMemo, useState } from "react";
 import { ModeTheme, ThemeContext, ThemePreference } from "@/providers/themeContext/themeContext";
 import { ThemeProvider } from "@mui/material/styles";
 import { generateAppTheme } from "@/styles/theme";
 import { getSystemTheme } from "@/lib/getSystemTheme";
 
 export const CustomThemeProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [didMount, setDidMount] = useState(false);
   const [mode, setMode] = useState<ModeTheme>(getSystemTheme());
+
+  useEffect(() => {
+    setDidMount(true);
+  }, []);
 
   const changeMode = useCallback(
     (newMode: ThemePreference) => {
@@ -27,7 +32,7 @@ export const CustomThemeProvider: FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={contextValue}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ThemeProvider theme={theme}>{didMount && children}</ThemeProvider>
     </ThemeContext.Provider>
   );
 };
