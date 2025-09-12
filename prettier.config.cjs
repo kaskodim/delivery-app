2b9659d66c4c5700dcb7bcee106b565dab470b36
DmitriyGulevich <kaskodim@gmail.com>
2025-09-12 15:08:53 +0300

    добавляет настройки сортировки классов Tailwind2 #60

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 120,
+  semi: true,
+  singleQuote: false,
+  singleAttributePerLine: true,
+};
